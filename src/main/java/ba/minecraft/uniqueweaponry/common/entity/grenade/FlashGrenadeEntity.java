757b73d2e0ef5e3c7b756bddf23539f31bc5f307
Admir Tuzović <admir.tuzovic@appimpact.ba>
2024-01-11 15:34:32 +0100

    Added applying of blindness on mobs around grenade splashing.

--- src/main/java/ba/minecraft/uniqueweaponry/common/entity/grenade/FlashGrenadeEntity.java
+++ src/main/java/ba/minecraft/uniqueweaponry/common/entity/grenade/FlashGrenadeEntity.java
@@ -1,10 +1,15 @@
 package ba.minecraft.uniqueweaponry.common.entity.grenade;
 
+import java.util.List;
+
 import ba.minecraft.uniqueweaponry.common.entity.GrenadeEntityTypes;
 import ba.minecraft.uniqueweaponry.common.entity.grenade.base.BaseGrenadeEntity;
 import ba.minecraft.uniqueweaponry.common.helpers.ModResourceLocation;
 import ba.minecraft.uniqueweaponry.common.item.GrenadeItems;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
@@ -12,6 +17,7 @@ import net.minecraft.world.entity.EntityType.Builder;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.HitResult;
 
 public final class FlashGrenadeEntity extends BaseGrenadeEntity {
@@ -53,10 +59,36 @@ public final class FlashGrenadeEntity extends BaseGrenadeEntity {
 	}
 
 	@Override
-	protected void onHit(HitResult pResult) {
+	protected void onHit(HitResult result) {
+		
+		// Get location where grenade hit the ground.
+		var hitLocation = result.getLocation();
+		
+		// Create boundaries of hit area.
+		AABB area = AABB.ofSize(hitLocation, 16, 16, 16);
+		
+		// Get list of all entities in the area.
+		List<Entity> entities = this.level().getEntities(this, area);
+		
+		// Iterate through list of all entities in the area.
+		for(Entity entity : entities) {
+			
+			// IF: Entity is living entity.
+			if(entity instanceof LivingEntity) {
+				
+				// Cast entity as living entity.
+				LivingEntity livingEntity = (LivingEntity) entity;
+				
+				// Create instance of blindness effect.
+				MobEffectInstance effectInstance = new MobEffectInstance(MobEffects.BLINDNESS, 5 * 20);
+				
+				// Apply it to mob.
+				livingEntity.addEffect(effectInstance);
+			}
+		}
 		
 		// Call mandatory base class code.
-		super.onHit(pResult);
+		super.onHit(result);
 		
 		
 	}
